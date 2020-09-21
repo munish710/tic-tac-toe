fbdee0b5910394942c5b688ebc83bcd97828bfc0
Munish Thakur <munish81198@gmail.com>
2020-09-21 21:58:26 +0530

    Updates

---
+++ precache-manifest.49382d2fa2b961c8106762fce6642055.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "3d4d7e71dc2bf542686d716709b50560",
+    "url": "/tic-tac-toe/index.html"
+  },
+  {
+    "revision": "2b8cb5887734085be2ea",
+    "url": "/tic-tac-toe/static/css/main.2549eaa6.chunk.css"
+  },
+  {
+    "revision": "9792fe6246ed720e89a6",
+    "url": "/tic-tac-toe/static/js/2.de32b394.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/tic-tac-toe/static/js/2.de32b394.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "2b8cb5887734085be2ea",
+    "url": "/tic-tac-toe/static/js/main.f7483ae5.chunk.js"
+  },
+  {
+    "revision": "8a5e5f2d838bbbbe6449",
+    "url": "/tic-tac-toe/static/js/runtime-main.5fc51fe3.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/tic-tac-toe/static/media/logo.5d5d9eef.svg"
+  }
+]);
